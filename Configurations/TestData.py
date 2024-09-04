3c21746cf684a4ce9df5b9135aed6848c32d7852
Artem <hitko21@gmail.com>
2024-09-04 14:04:28 +0300

    day two

---
+++ Configurations/TestData.py
@@ -0,0 +1,7 @@
+class TestData:
+    URL = 'https://www.i.ua/'
+    LOGIN = 'ukr_test_qa@i.ua'
+    WRONG_LOGIN = 'wrong_login'
+    PASSWORD = 'Ukraine1991'
+    WRONG_PASSWORD = 'wrong_password'
+    TEXT_TO_SEARCH = 'Python'
