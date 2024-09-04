e498dfa8f25e3fb4ed191a9a23fb520dfcb340ed
Artem <hitko21@gmail.com>
2024-09-04 11:13:47 +0300

    day two

--- Pages/MainPage.py
+++ Pages/MainPage.py
@@ -6,18 +6,34 @@ class MainPage:
     def __init__(self, driver: WebDriver):
         self.driver = driver
 
-        self.textbox_username = (By.XPATH, "//input[@name='login']")
+        self.textbox_usermail = (By.XPATH, "//input[@name='login']")
         self.textbox_password = (By.XPATH, "//input[@name='pass']")
-        self.submit_button = (By.XPATH, "//input[@title='Вхід на пошту']")
+        self.submit_button_main = (By.XPATH, "//input[@title='Вхід на пошту']")
+        self.button_enter_popup = (By.XPATH, "//a[contains(text(),'Вхід')]")
+        self.textbox_usermail_in_popup = (By.XPATH, "//form[@name='lFloat']//input[@name='login']")
+        self.textbox_password_in_popup = (By.XPATH, "//form[@name='lFloat']//input[@name='pass']")
+        self.submit_button_enter_popup = (By.XPATH, "//form[@name='lFloat']//input[@value='Увійти']")
 
     def fill_login_field(self, username):
-        self.driver.find_element(*self.textbox_username).send_keys(username)
+        self.driver.find_element(*self.textbox_usermail).send_keys(username)
 
-    def fiil_password_field(self, password):
+    def fill_login_field_in_popup(self, usermail):
+        self.driver.find_element(*self.textbox_usermail_in_popup).send_keys(usermail)
+
+    def fill_password_field(self, password):
         self.driver.find_element(*self.textbox_password).send_keys(password)
 
-    def click_submit_button(self):
-        self.driver.find_element(*self.submit_button).click()
+    def fill_password_field_in_popup(self, password):
+        self.driver.find_element(*self.textbox_password_in_popup).send_keys(password)
+
+    def click_submit_button_main(self):
+        self.driver.find_element(*self.submit_button_main).click()
+
+    def click_submit_button_enter_popup(self):
+        self.driver.find_element(*self.button_enter_popup).click()
+
+    def click_submit_button_in_popup(self):
+        self.driver.find_element(*self.submit_button_enter_popup).click()
 
     def get_pageTitle(self):
         return self.driver.title
--- Tests/test_login.py
+++ Tests/test_login.py
@@ -11,7 +11,41 @@ class TestLogin:
 
         main_page = MainPage(driver)
         main_page.fill_login_field(TestData.LOGIN)
-        main_page.fiil_password_field(TestData.PASSWORD)
-        main_page.click_submit_button()
+        main_page.fill_password_field(TestData.PASSWORD)
+        main_page.click_submit_button_main()
 
         assert main_page.get_pageTitle() == 'Вхідні -\xa0I.UA\xa0'
+
+    def test_with_wrong_login(self, firefox):
+        driver = firefox
+        driver.get(TestData.URL)
+
+        main_page = MainPage(driver)
+        main_page.fill_login_field(TestData.WRONG_LOGIN)
+        main_page.fill_password_field(TestData.PASSWORD)
+        main_page.click_submit_button_main()
+
+        assert main_page.get_pageTitle() == 'Паспорт -\xa0I.UA\xa0'
+
+    def test_with_wrong_password(self, firefox):
+        driver = firefox
+        driver.get(TestData.URL)
+
+        main_page = MainPage(driver)
+        main_page.fill_login_field(TestData.WRONG_LOGIN)
+        main_page.fill_password_field(TestData.WRONG_PASSWORD)
+        main_page.click_submit_button_main()
+
+        assert main_page.get_pageTitle() == 'Паспорт -\xa0I.UA\xa0'
+
+    def test_success_loin_with_popup(self, firefox):
+        driver = firefox
+        driver.get(TestData.URL)
+
+        main_page = MainPage(driver)
+        main_page.click_submit_button_enter_popup()
+        main_page.fill_login_field_in_popup(TestData.LOGIN)
+        main_page.fill_password_field_in_popup(TestData.PASSWORD)
+        main_page.click_submit_button_in_popup()
+
+        assert main_page.get_pageTitle() == 'І.UA - моя пошта\xa0'
